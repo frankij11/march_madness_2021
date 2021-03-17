46e6f22a2146af0a4e45312a1c98c2c122b08512
frankij11 <kevinfjoy@gmail.com>
2021-03-17 19:08:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import setuptools
 from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as fh:
+    depends = fh.read()
+
 
 setup(
     name='march_madness_2021',
     packages=find_packages(),
     version='0.1.0',
     description='Bracketology like prediction algoirthm for 2021 NCAA Basketall Tournament',
+    install_requires = depends,
+    package_data={"": ['data/*']},
     author='Kevin Joy',
     license='MIT',
 )
