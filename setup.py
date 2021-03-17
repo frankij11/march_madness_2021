acbdbecda55ed7980ff0bde4a4d4fb442642c907
frankij11 <kevinfjoy@gmail.com>
2021-03-17 19:04:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='march_madness_2021',
     packages=find_packages(),
     version='0.1.0',
     description='Bracketology like prediction algoirthm for 2021 NCAA Basketall Tournament',
